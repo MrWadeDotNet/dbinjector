21e23acdb6ed06f33bd5d531f100eed9296a7e22
danielcossins11 <daniel.cossins@yahoo.com>
2015-09-04 09:29:23 -0500

    modified for beer app

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 $(".buttonclick").on("click", function() {
 
-var userInput = $("#getmovieid").val();
-var getDbLink = $("#databaselink").val();
+// var userInput = $("#getmovieid").val();
+// var getDbLink = $("#databaselink").val();
 
-console.log(userInput);
-console.log(getDbLink);
+// console.log(userInput);
+// console.log(getDbLink);
 
-$.ajax({
-        url:"http://www.omdbapi.com/?i=" + userInput,
+      $.ajax({
+        // url:"http://www.omdbapi.com/?i=" + userInput,
         //method: "getmovieid"
+        url: "http://api.brewerydb.com/v2/?key=124796ba126c92f04f87e154a597c112"
       }).done(function(data){
-        console.log("done fired");
-        console.log(data);
+        // console.log("done fired");
+        // console.log(data);
 
-        data.poster = "http://img.omdbapi.com/?i=" + data.imdbID + "&apikey=8513e0a1";
-        console.log("done");
-        $.ajax({
-          url: getDbLink,
-          method: "POST",
-          data: JSON.stringify(data)
-        }).done(function(){
-          console.log("injected");
+        // data.poster = "http://img.omdbapi.com/?i=" + data.imdbID + "&apikey=8513e0a1";
+        // console.log("done");
+        // $.ajax({
+        //   url: getDbLink,
+        //   method: "POST",
+        //   data: JSON.stringify(data)
+        console.log(data);
         });
-      });
 
 
 }); 
